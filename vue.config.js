5cfeefd03b01b9ad6ed58afd08c123c15d9492d5
weihu <1183969348@qq.com>
2019-07-26 18:46:45 +0800

    init3

--- vue.config.js
+++ vue.config.js
@@ -1,60 +1,128 @@
 const path = require('path');
- 
-function resolve(dir) {
-  return path.join(__dirname, dir)
-}
- 
-// 导入compression-webpack-plugin
 
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const externals = {
-  'vue': 'Vue',
-  'vue-router': 'VueRouter',
-  'vuex': 'Vuex',
-  'axios': 'axios',
-  'ELEMENT': 'element-ui'
-}
-// 定义压缩文件类型
-const productionGzipExtensions = ['js', 'css']
- 
+
+ 
+
 module.exports = {
-  publicPath: '/vue-qiugu-ms/', //基本路径
-  outputDir: 'dist',
-  productionSourceMap: false,
-  assetsDir: 'static',
-  filenameHashing: true,
-  pages: {
-    index: {
-      // page 的入口
-      entry: "src/main.js",
-      // 模板来源
-      template: "public/index.html", // 这里用来区分加载那个 html
-      // 在 dist/index.html 的输出
-      filename: "index.html",
-      // 在这个页面中包含的块，默认情况下会包含
-      // 提取出来的通用 chunk 和 vendor chunk。
-      chunks: ["chunk-vendors", "chunk-common", "index"]
-    }
-  },
 
-  // CSS 相关选项
-  css: {
-    extract: true,
-    sourceMap: false,
-    loaderOptions: {}, // 为所有的 CSS 及其预处理文件开启 CSS Modules。
-    modules: false
-  },
-  // 在多核机器下会默认开启。
-  parallel: require('os').cpus().length > 1,
-  // PWA 插件的选项。
-  pwa: {},
-  // 配置 webpack-dev-server 行为。
-  devServer: {
-    port: 3001,
-    open: true,
-    proxy: 'http://localhost:8080'
-  },
-  
-  // 第三方插件的选项
-  pluginOptions: {}
-}
+  //基本路径
+
+  baseUrl: './',
+
+  //输出文件目录
+
+  outputDir: 'dist',
+
+  // eslint-loader 是否在保存的时候检查
+
+  lintOnSave: false,
+
+  //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+
+  assetsDir: 'static',
+
+  //以多页模式构建应用程序。
+
+  pages: undefined,
+
+  //是否使用包含运行时编译器的 Vue 构建版本
+
+  runtimeCompiler: false,
+
+  //是否为 Babel 或 TypeScript 使用 thread-loader。该选项在系统的 CPU 有多于一个内核时自动启用，仅作用于生产构建，在适当的时候开启几个子进程去并发的执行压缩
+
+  parallel: require('os').cpus().length > 1,
+
+  //生产环境是否生成 sourceMap 文件，一般情况不建议打开
+
+  productionSourceMap: false,
+
+  // webpack配置
+
+  //对内部的 webpack 配置进行更细粒度的修改 https://github.com/neutrinojs/webpack-chain see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+
+  chainWebpack: config => {
+
+    /**
+
+     * 删除懒加载模块的prefetch，降低带宽压力
+
+     * https://cli.vuejs.org/zh/guide/html-and-static-assets.html#prefetch
+
+     * 而且预渲染时生成的prefetch标签是modern版本的，低版本浏览器是不需要的
+
+     */
+
+    //    config.plugins.delete('prefetch');
+
+    //    if(process.env.NODE_ENV === 'production') { // 为生产环境修改配置...process.env.NODE_ENV !== 'development'
+
+    //    } else {// 为开发环境修改配置...
+
+    //    }
+
+  },
+
+  //调整 webpack 配置 https://cli.vuejs.org/zh/guide/webpack.html#%E7%AE%80%E5%8D%95%E7%9A%84%E9%85%8D%E7%BD%AE%E6%96%B9%E5%BC%8F
+
+  
+  css: {
+
+    // 启用 CSS modules
+
+    modules: false,
+
+    // 是否使用css分离插件
+
+    extract: true,
+
+    // 开启 CSS source maps，一般不建议开启
+
+    sourceMap: false,
+
+    // css预设器配置项
+
+    loaderOptions: {
+
+      sass: {
+
+        //设置css中引用文件的路径，引入通用使用的scss文件（如包含的@mixin）
+
+        data: `
+
+        $baseUrl: "/";
+
+      `
+
+      }
+
+    }
+
+  },
+
+  // webpack-dev-server 相关配置 https://webpack.js.org/configuration/dev-server/
+
+  devServer: {
+
+    host: 'localhost',
+
+   
+
+    port: 8000, // 端口号
+
+    https: false, // https:{type:Boolean}
+
+    open: true, //配置自动启动浏览器 http://XXX.XXX.X.XX:7071/rest/XXX/
+
+    hotOnly: true, // 热更新
+
+    proxy: 'http://localhost:8000' // 配置跨域处理,只有一个代理
+
+   
+
+  },
+
+ 
+
+
+};
